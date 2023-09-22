792fd6893947120cb6d124b31dfd73d533979882
Gotcha190 <konradformella190@gmail.com>
2023-09-22 22:35:33 +0200

    first reasonable commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '5280a580554f24d53f8b');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '67ccea4b00c7fd6d316a');
